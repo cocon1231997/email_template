230c7d50f1bb2217c4e4b88ed5ef77c128131499
Quang Hieu <quanghieu10011997@gmail.com>
2020-09-12 16:40:55 +0700

    push code email_template

---
+++ home/email_teamplate.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Email template</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+    <header>
+        <div class="menu_header">
+            <div class="container">
+                <div class="logo">
+                    <img src="../images/logo.png" alt="Logo">
+                    <p>Focus</p>
+                </div>
+                <div class="menu">
+                    <p>LIFESTYLE ACCESSORIES WITH A DOWN TO EARTH</p>
+                </div>
+            </div>
+        </div>
+    </header>
+    <div id="main">
+        <h1>ONE BAG, 2PACK, THREE WAYS.</h1>
+        <div class="box-header">
+            <div class="item">
+                <div class="item-img">
+                    <img src="../images/img1.png" alt="img1">
+                </div>
+            </div>
+            <div class="item">
+                <div class="item-img">
+                    <img src="../images/img2.png" alt="img2">
+                </div>
+                <div class="item-content">
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.</p>
+                </div>
+                <div class="item-button">
+                    <a href="#">iphone now <span><img src="../images/arrow.png" alt=""></span></a>
+                </div>
+            </div>
+            <div class="item">
+                <div class="item-img">
+                    <img src="../images/img3.png" alt="img3">
+                </div>
+                <div class="item-content">
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.</p>
+                </div>
+                <div class="item-button">
+                    <a href="#">iphone now <span><img src="../images/arrow.png" alt=""></span></a>
+                </div>
+            </div>
+        </div>
+        <p class="title">iphone cases</p>
+        <div class="box-cases">
+            <div class="case-item">
+                <div class="case-img">
+                    <img src="../images/img4.png" alt="img4">
+                </div>
+                <div class="case-content">
+                    <p class="case-name">SPORT IPHONE 5 CASE</p>
+                    <p class="case-description">HERITAGE</p>
+                </div>
+            </div>
+            <div class="case-item">
+                <div class="case-img">
+                    <img src="../images/img4.png" alt="img4">
+                </div>
+                <div class="case-content">
+                    <p class="case-name">SPORT IPHONE 5 CASE</p>
+                    <p class="case-description">HERITAGE</p>
+                </div>
+            </div>
+            <div class="case-item">
+                <div class="case-img">
+                    <img src="../images/img4.png" alt="img4">
+                </div>
+                <div class="case-content">
+                    <p class="case-name">SPORT IPHONE 5 CASE</p>
+                    <p class="case-description">HERITAGE</p>
+                </div>
+            </div>
+        </div>
+        <div class="item-button case-loadmore">
+            <a href="#">iphone now <span><img src="../images/arrow.png" alt=""></span></a>
+        </div>
+        <div class="box-bottom">
+            <div class="box-item-banner">
+                <p class="banner-title">CLASSIC HG LAPTOP FOLIO HERITAGE</p>
+                <a href="#">CLICK FOR SIZE AND SPECS</a>
+            </div>
+            <div class="box-item-des">
+                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage.</p>
+                <a href="#">Read more</a>
+            </div>
+        </div>
+    </div>
+    <div id="footer">
+        <p>Terms | Privacy | Unsubscribe</p>
+    </div>
+</body>
+
+</html>
